6076207f655bcdafa6cfd76ff407ae0f30603878
maikully <zeebli123@gmail.com>
2022-11-15 16:58:40 -0500

    working?

--- base.py
+++ base.py
@@ -79,10 +79,10 @@ def process_file():
     #print(strs)
     os.remove(UPLOAD_FOLDER + "/" + filename)
     return jsonify(data=strs)
-    
+
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
